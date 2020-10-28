7a8f7993118ceef46963e5910f995a2f841932c5
Mario Campopiano <mcampopiano92@gmail.com>
2020-10-28 10:54:18 -0500

    Working on event listener for the details button in attractionlist.js. left notes on line 25 for where to pick back up

--- scripts/attractions/AttractionsList.js
+++ scripts/attractions/AttractionsList.js
@@ -8,16 +8,29 @@ const contentContainer = document.querySelector("#attractionCard")
 const attractionCardHtml = (attractionObj) => {
     contentContainer.innerHTML = `
         <h2>${attractionObj.name}</h2>
-            <button>Details</button>
+            <button id="attractionDetails--${attractionObj.id}">Details</button>
     `
 }
 
 // Listens for the change event from attractionSelect.js
 eventHub.addEventListener("chosenAttraction", event => {
-    if (event.target.id === "tripDropDown__attraction") {
-        const attractionId = event.target.value
+        const attractionId = event.detail.chosenAttraction
         const attractionArray = useAttractions()
         const chosenAttraction = attractionArray.find(attractionObj => attractionObj.id === attractionId)
         attractionCardHtml(chosenAttraction)
+})
+
+
+// Listens for the click on the attraction card button then dispatches custom event
+// When you come back, do the split on the button id to find the attraction id and add it to line 31
+eventHub.addEventListener("click", clickEvent => {
+    if (clickEvent.target.id.startsWith("attractionDetails")) {
+        console.log("I'm broadcasting!")
+        const detailBtnClicked = new CustomEvent("attractionDetailsRequested", {
+            detail: {
+                attractionId: 2
+            }
+        })
+        eventHub.dispatchEvent(detailBtnClicked)
     }
 })
--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,7 @@ import { getParks } from "./parks/ParkProvider.js";
 import "./parks/ParkList.js"
 import "./attractions/AttractionsSelect.js"
 import { getAttractions } from "./attractions/AttractionProvider.js";
+import "./attractions/AttractionsList.js"
 getParks();
 getAttractions()
 console.log("Hey good lookin'")
