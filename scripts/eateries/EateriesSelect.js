94390c3e9214fe969cbada9850b2be33f5e0ad6e
Jasmin Kaset <kasetski@gmail.com>
2020-10-28 10:37:07 -0500

    importing in to main

--- scripts/eateries/EateriesSelect.js
+++ scripts/eateries/EateriesSelect.js
@@ -12,17 +12,18 @@ export const EateriesSelect = () => {
       const eateriesArray = useEateries();
       contentTarget.innerHTML = eateriesArray.map(eatery => 
       `<option value=0>Please select an eatery</option>
-      <option value=${}>${eatery.businessName}</option>`)
+      <option value = ${eatery.id}>${eatery.businessName}</option>`)
   })
 }
 
 eventHub.addEventListener("change", (changeEvent) => {
   if (changeEvent.target.id === "tripDropDown__eatery") {
     //console.log("change in dropdown")
-    const eaterySelectEvent = new CustomEvent("chosenEatery", {
+    const eatery = new CustomEvent("chosenEatery", {
       detail: {
-        
+        chosenEatery: parseInt(changeEvent.target.value)
       }
     })
+    eventHub.dispatchEvent(eatery)
   }
 })
--- scripts/main.js
+++ scripts/main.js
@@ -5,3 +5,10 @@ import { getAttractions } from "./attractions/AttractionProvider.js";
 getParks();
 getAttractions()
 console.log("Hey good lookin'")
+
+
+import { getEateries } from "./eateries/EateryProvider.js"
+import { EateriesSelect } from "./eateries/EateriesSelect.js";
+ 
+EateriesSelect()
+getEateries()
