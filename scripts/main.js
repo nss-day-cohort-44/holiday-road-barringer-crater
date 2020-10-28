58bca4c65e8105b038275343b38414db62e3274e
Jasmin Kaset <kasetski@gmail.com>
2020-10-28 10:18:41 -0500

    main.js changes saved

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-// import { getParks } from "./parks/ParkSelect.js";
-// import "./parks/ParkList.js"
-
-// getParks();
-// console.log("Hey good lookin'")
-
-
-import { EateriesSelect} from "./eateries/EateriesSelect.js"
-
-import { getEateries, useEateries } from "./eateries/EateryProvider.js";
-getEateries()
-useEateries()
-EateriesSelect()
-=======
 import { getParks } from "./parks/ParkProvider.js";
 import "./parks/ParkList.js"
 import "./attractions/AttractionsSelect.js"
@@ -20,4 +5,3 @@ import { getAttractions } from "./attractions/AttractionProvider.js";
 getParks();
 getAttractions()
 console.log("Hey good lookin'")
->>>>>>> master
