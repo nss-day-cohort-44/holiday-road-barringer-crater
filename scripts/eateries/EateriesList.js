655c01ade83399e03a1e9f7280395fda78fd5900
Jasmin Kaset <kasetski@gmail.com>
2020-10-28 11:56:07 -0500

    EateriesList.js

---
+++ scripts/eateries/EateriesList.js
@@ -0,0 +1,12 @@
+import {useEateries} from "./EateryProvider.js"
+
+const eventHub = document.querySelector(".container")
+const contentTarget = document.querySelector("#tripDropDown__eatery")
+
+//create single HTML 'card'
+
+// needs to listen to the eventHub
+
+// a function that gets exported so any module can execute it
+
+// the function that renders HTML to the DOM 
