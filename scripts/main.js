639664716fe421eb7d227e581ecae4f27ba35c80
dalamcd <dalamcd@gmail.com>
2020-10-28 10:18:54 -0500

    fixed merge conflicts

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,8 @@
 import { getParks } from "./parks/ParkProvider.js";
 import "./parks/ParkSelect.js"
 import "./parks/ParkList.js"
-<<<<<<< HEAD
-
-
-=======
 import "./attractions/AttractionsSelect.js"
 import { getAttractions } from "./attractions/AttractionProvider.js";
->>>>>>> master
 getParks();
 getAttractions()
 console.log("Hey good lookin'")
