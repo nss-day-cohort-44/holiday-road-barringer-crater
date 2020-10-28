750c9e89cf9ce6ee9f72d2dc413d18bba987bdb4
Mario Campopiano <mcampopiano92@gmail.com>
2020-10-28 13:54:08 -0500

    Added comments to the code

--- scripts/attractions/AttractionsDetail.js
+++ scripts/attractions/AttractionsDetail.js
@@ -1,9 +1,12 @@
-import { useAttractions } from "./AttractionProvider.js";
+import { useAttractions } from "./AttractionProvider.js"
 
-const eventHub = document.querySelector(".container");
+// Targets the main element as the eventHub
+const eventHub = document.querySelector(".container")
+// Targets the currently empty div inside the attraction card
+const detailContainer = document.querySelector("#attraction--details")
 
 eventHub.addEventListener("attractionDetailsRequested", event => {
+    // Goes through the attractions array and returns the object with the matching id from the attraction selected in the dropdown (see AttractionList.js for the detail id)
     const chosenAttraction = useAttractions().find(attractionObj => attractionObj.id === event.detail.attractionId)
-    const detailContainer = document.querySelector("#attraction--details");
     detailContainer.innerHTML = chosenAttraction.description;
 });
