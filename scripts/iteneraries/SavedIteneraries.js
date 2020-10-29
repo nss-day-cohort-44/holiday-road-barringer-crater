f3e27a2347e9ea644033a01dedd9edc36ec4de3b
Mario Campopiano <mcampopiano92@gmail.com>
2020-10-29 10:15:09 -0500

    Pushing up itineraryDataProvider.js

--- scripts/iteneraries/SavedIteneraries.js
+++ scripts/iteneraries/SavedIteneraries.js
@@ -1,2 +1,15 @@
 import {useItineraries} from "./itineraryDataProvider.js"
+import {useParks} from "../parks/ParkProvider.js"
 
+const contentContainer = document.querySelector(".savedItineraries")
+
+const render = (itineraryObj) => {
+    contentContainer.innerHTML += `
+    <h2>${itineraryObj.fullName}</h2>
+    `
+}
+
+export const SavedItinerariesList = () => {
+    const parksArr = useParks()
+
+}
--- scripts/iteneraries/itineraryDataProvider.js
+++ scripts/iteneraries/itineraryDataProvider.js
@@ -1,7 +1,10 @@
-let savedItineraries = ""
+// Empty array to eventually store the objects from the local api
+let savedItineraries = []
 
+// returns a copy of the savedItineraries array so that the original won't be affected
 export const useItineraries = () => savedItineraries.slice()
 
+// get's the objects from the local api, converts them to json readable and adds them to the savedItineraries array
 export const getItineraries = () => {
     return fetch("http://localhost:8088/itineraries")
     .then(response => response.json())
