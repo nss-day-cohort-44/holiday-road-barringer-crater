422db23ea8c70ac6e3aff1b010f99b462cff9363
Your Github User Name <david.lav.williams@gmail.com>
2020-10-30 11:49:39 -0500

    the selected eateries are now rendered to the dom on button click

--- scripts/iteneraries/workingItinerary.js
+++ scripts/iteneraries/workingItinerary.js
@@ -1,4 +1,5 @@
 import { useAttractions } from "../attractions/AttractionProvider.js"
+import { useEateries } from "../eateries/EateryProvider.js"
 
 //attractionAdded
 const eventHub = document.querySelector(".container")
@@ -7,14 +8,14 @@ const eventHub = document.querySelector(".container")
 //to put selections in working aside
 export const dispatchWorkingItinerary = () => {
   let workingItinerariesArray=[]
+  const contentTarget = document.querySelector(".workingItineraries")
+
   eventHub.addEventListener("attractionAdded", e => {
-    //console.log("hi")
-    const contentTarget = document.querySelector(".workingItineraries")
-    const attractionsArray = useAttractions()
+    
+    if (e.detail.attractionId !== 0) {
+      const attractionsArray = useAttractions()
     
     const currentAttraction = attractionsArray.find(attractionObj => attractionObj.id === e.detail.attractionId)
-    // console.log(currentAttraction)
-    if (e.detail.attractionId !== 0) {
       contentTarget.innerHTML += `
         <h3>${currentAttraction.name}</h3>
       `
@@ -22,24 +23,28 @@ export const dispatchWorkingItinerary = () => {
         id:currentAttraction.id,
         type:"attraction"
       })
-      // console.log(workingItinerariesArray)
+      console.log(workingItinerariesArray)
     }
     //ends if
   })//ends eventHub
+  eventHub.addEventListener("eateryAdded", e=>{
+    console.log("hi")
+    if (e.detail.eateryId !==0) {
+      const eateriesArray=useEateries()
+      
+      const currentEatery=eateriesArray.find(eateryObj => eateryObj.id=== e.detail.eateryId)
+      contentTarget.innerHTML+=
+      `
+        <h3>${currentEatery.businessName}</h3>
+      `
+      workingItinerariesArray.push({
+        id:currentEatery.id,
+        type:"eatery"
+      })
+    }
+
+  })
+
+
 } //ends export
 
-//parksArr.find(parkObj => parkObj.id === itinerary.parkId)
-
-// eventHub.addEventListener("change", event => {
-//   if (event.target.id === "tripDropDown__attractions") {
-//       console.log("I'm broadcasting")
-//       const attraction = new CustomEvent("chosenAttraction", {
-//           detail: {
-//               chosenAttraction: parseInt(event.target.value)
-//           }
-//       })
-//       // console.log(attraction)
-//       eventHub.dispatchEvent(attraction)
-//   }
-// })
-// }
