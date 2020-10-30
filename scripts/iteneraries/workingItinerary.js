67c30357401dfe1fedb275391581149c9606aae4
Your Github User Name <david.lav.williams@gmail.com>
2020-10-30 11:28:17 -0500

    creates an object that is pushed into an array

--- scripts/iteneraries/workingItinerary.js
+++ scripts/iteneraries/workingItinerary.js
@@ -6,18 +6,25 @@ const eventHub = document.querySelector(".container")
 
 //to put selections in working aside
 export const dispatchWorkingItinerary = () => {
+  let workingItinerariesArray=[]
   eventHub.addEventListener("attractionAdded", e => {
     //console.log("hi")
     const contentTarget = document.querySelector(".workingItineraries")
     const attractionsArray = useAttractions()
     
     const currentAttraction = attractionsArray.find(attractionObj => attractionObj.id === e.detail.attractionId)
-    //console.log(currentAttraction)
+    // console.log(currentAttraction)
     if (e.detail.attractionId !== 0) {
       contentTarget.innerHTML += `
         <h3>${currentAttraction.name}</h3>
       `
-    }//ends if
+      workingItinerariesArray.push({
+        id:currentAttraction.id,
+        type:"attraction"
+      })
+      // console.log(workingItinerariesArray)
+    }
+    //ends if
   })//ends eventHub
 } //ends export
 
