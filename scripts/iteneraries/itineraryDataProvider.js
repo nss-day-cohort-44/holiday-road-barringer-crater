5a64672c65f079b0aea991698b03437d4ae0f136
Your Github User Name <david.lav.williams@gmail.com>
2020-11-02 13:50:23 -0600

    its not fully  working yet

--- scripts/iteneraries/itineraryDataProvider.js
+++ scripts/iteneraries/itineraryDataProvider.js
@@ -3,8 +3,8 @@ import { useWorkingAttractions, useWorkingIteneraries } from "./workingItinerary
 
 // Empty array to eventually store the objects from the local api
 let savedItineraries = []
-let savedItineraryEateries=[]
-let savedItineraryAttractions=[]
+let savedItineraryEateries = []
+let savedItineraryAttractions = []
 
 const eventHub = document.querySelector(".container");
 
@@ -14,21 +14,23 @@ export const useItineraries = () => savedItineraries.slice()
 // get's the objects from the local api, converts them to json readable and adds them to the savedItineraries array
 export const getItineraries = () => {
     return fetch("http://localhost:8088/itineraries")
-    .then(response => response.json())
-    .then(parsedItineraries => savedItineraries = parsedItineraries)
+        .then(response => response.json())
+        .then(parsedItineraries => savedItineraries = parsedItineraries)
 }
 
-export const getItinerariesEateries=()=>{
+export const getItinerariesEateries = () => {
     return fetch("http://localhost:8088/itineraryEatery")
-    .then(response => response.json())
-    .then(parsedItineraries => savedItineraryEateries = parsedItineraries)
+        .then(response => response.json())
+        .then(parsedItineraries => savedItineraryEateries = parsedItineraries)
 }
 
-export const getItinerariesAttractions=()=>{
+export const getItinerariesAttractions = () => {
     return fetch("http://localhost:8088/itineraryAttraction")
-    .then(response => response.json())
-    .then(parsedItineraries => savedItineraryAttractions = parsedItineraries)
+        .then(response => response.json())
+        .then(parsedItineraries => savedItineraryAttractions = parsedItineraries)
 }
+
+
 
 // push park into itineraryArray
 export const saveItinerary = (itinObj) => {
@@ -39,12 +41,12 @@ export const saveItinerary = (itinObj) => {
         },
         body: JSON.stringify(itinObj)
     })
-    .then(getItineraries)
-    .then(readableObjects)
+        // .then(getItineraries)
+        // .then(readableObjects)
 }
 // push the Itineraries Attraction into that array
-export const saveItineraryAttraction=(itinObj)=>{
-    return fetch ("http://localhost:8088/itineraryAttraction", {
+export const saveItineraryAttraction = (itinObj) => {
+    return fetch("http://localhost:8088/itineraryAttraction", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
@@ -53,25 +55,25 @@ export const saveItineraryAttraction=(itinObj)=>{
     })
 }
 // push the Itineraries Eatery into that array
-export const saveItineraryEatery=()=>{
-    then( fetch ("http://localhost:8088/itineraryEatery", {
+export const saveItineraryEatery = (itinObj) => {
+    return fetch("http://localhost:8088/itineraryEatery", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
         },
         body: JSON.stringify(itinObj)
     })
-    )
+    
 }
 
 
 
 eventHub.addEventListener("click", e => {
-    if(e.target.id === "itinerarySave" && e.target.classList.contains("buttonSelectable")) {
+    if (e.target.id === "itinerarySave" && e.target.classList.contains("buttonSelectable")) {
         const itineraryObj = {
-            itenerary: useWorkingIteneraries()
-        } 
-        
+            testing: "Is this going to push?"
+        }
+
         saveItinerary(itineraryObj);
         saveItineraryAttraction(itineraryObj)
         saveItineraryEatery(itineraryObj)
