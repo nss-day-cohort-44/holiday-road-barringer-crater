c1a53619a6c77a772f81bc1dcd53790869bc8b2f
Mario Campopiano <mcampopiano92@gmail.com>
2020-11-03 09:22:08 -0600

    The fetch call to the api seems to be working, waiting on the saved itineraries to render so I can test functionality

--- scripts/events/EventDataProvider.js
+++ scripts/events/EventDataProvider.js
@@ -6,8 +6,8 @@ export const useEvents = () => {
     return events.data.slice()
 }
 
-export const getEvents = () => {
-    return fetch(`https://developer.nps.gov/api/v1/events?api_key=${keys.npsKey}&pageSize=2`)
+export const getEvents = (parkCode) => {
+    return fetch(`https://developer.nps.gov/api/v1/events?&${parkCode}api_key=${keys.npsKey}&pageSize=2`)
     .then(res => res.json())
     .then(parsedEvents => events = parsedEvents)
 }
