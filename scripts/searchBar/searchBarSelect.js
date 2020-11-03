2e0efe0d7e5640a6dccbee989c660a2fecb81d0c
dalamcd <dalamcd@gmail.com>
2020-11-03 10:20:05 -0600

    Added search functinality for parks, eateries and attractions

--- scripts/searchBar/searchBarSelect.js
+++ scripts/searchBar/searchBarSelect.js
@@ -1,33 +1,38 @@
 import { useAttractions } from "../attractions/AttractionProvider.js";
+import { useEateries } from "../eateries/EateryProvider.js";
+import { useParks } from "../parks/ParkProvider.js"
 import { useItineraries } from "../iteneraries/itineraryDataProvider.js";
 
-export const dispatchSearchBarSelect=()=>{
-    const contentTarget=document.querySelector(".searchBar__output")
-    document.querySelector("#searchBar")
-    .addEventListener("keypress", keyPressEvent => {
+export const dispatchSearchBarSelect = () => {
+
+    const searchFunction = (keyPressEvent) => {
+        let foundItems = [];
+        foundItems = foundItems.concat(useEateries().filter(searchedItem =>
+            searchedItem.description.includes(keyPressEvent.target.value)));
+        foundItems.forEach(item => {
+            item.name = item.businessName;
+        })
+        foundItems = foundItems.concat(useAttractions().filter(searchedItem =>
+            searchedItem.description.includes(keyPressEvent.target.value)));
+        foundItems = foundItems.concat(useParks().filter(searchedItem =>
+            searchedItem.description.includes(keyPressEvent.target.value)));
+        return foundItems;
+    }
+
+    const contentTarget = document.querySelector(".searchBar__output")
+    document.querySelector("#searchBar").addEventListener("keypress", keyPressEvent => {
         if (keyPressEvent.charCode === 13) {
-            const searchFunction=()=>{
-            const attractionArray = useAttractions()    
-            let foundItem = attractionArray.filter(searchedItem => searchedItem.description.includes(keyPressEvent.target.value))
-            return foundItem
-            }
-
-            let listOfFoundItem=searchFunction()
+            let listOfFoundItem = searchFunction(keyPressEvent)
             console.log(listOfFoundItem)
-            let foundItemHTML=""
-            for (const item of listOfFoundItem){
-                foundItemHTML+=render(item)
+            let foundItemHTML = ""
+            for (const item of listOfFoundItem) {
+                foundItemHTML += render(item)
             }
-            contentTarget.innerHTML =foundItemHTML
-
-
-            
+            contentTarget.innerHTML = foundItemHTML
         }
     });
-   
 }
 
-const render=(foundObj)=>{
-
+const render = (foundObj) => {
     return `<p>${foundObj.name}</p>`
 }
